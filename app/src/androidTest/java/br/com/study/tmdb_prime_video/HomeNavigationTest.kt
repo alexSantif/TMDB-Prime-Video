f6881f4b5e6bf76d35a6377eec058316a77a64ba
alexSantif <alexsantos2102@gmail.com>
2024-06-20 08:13:28 -0300

    ajuste testes espresso

--- app/src/androidTest/java/br/com/study/tmdb_prime_video/HomeNavigationTest.kt
+++ app/src/androidTest/java/br/com/study/tmdb_prime_video/HomeNavigationTest.kt
@@ -1,34 +1,45 @@
 package br.com.study.tmdb_prime_video
 
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import br.com.study.tmdb_prime_video.home.R
-import br.com.study.tmdb_prime_video.home.presentation.HomeFragment
-import io.mockk.mockk
+import br.com.study.tmdb_prime_video.presentation.MainActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class HomeNavigationTest {
 
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun test_homeTabsNavigation() {
 
-        val mockNavController: NavController = mockk(relaxed = true)
-
-        val titleScenario = launchFragmentInContainer<HomeFragment>()
-
-        titleScenario.onFragment { fragment ->
-            Navigation.setViewNavController(fragment.requireView(), mockNavController)
+        activityRule.scenario.onActivity { activity ->
+            val navHostFragment = NavHostFragment.create(R.navigation.tmdb_navigation)
+            activity.supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, navHostFragment)
+                .setPrimaryNavigationFragment(navHostFragment)
+                .commitNow()
         }
 
-        onView(withId(R.id.iv_app_logo_topbar))
-            .check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_home)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testFragmentNavigation() {
+        activityRule.scenario.onActivity { activity ->
+            val navController = Navigation.findNavController(activity, R.id.fragmentContainerView)
+            navController.navigate(R.id.fragment_home)
+        }
+
+        onView(withId(R.id.fragment_home)).check(matches(isDisplayed()))
     }
 }
